cd8e023b0d95bae3302c3e67a24f91afc508f9c8
sgassefa <sgassefa@gmail.com>
2019-04-22 15:47:44 -0600

    my first assignment

---
+++ Assignment-1.R
@@ -0,0 +1,12 @@
+# first-repo
+
+#Part 1. Introductions
+my.age <- 45 #replace your age with ??
+my.name <- "shimelis" #replace your age with ??
+
+MakeIntroduction <- c(the above two variables separated by comma)
+
+MakeIntroduction <- paste() #Use paste function to string together "Hello, my name is", my.name, "and I am", my.age, "years old" together in the order lsitsed separated by comma.
+
+MakeIntroduction <- paste("Hello, my name is", my.name, "and I am", my.age, "years old") 
+#Use paste function to string together "Hello, my name is", my.name, "and I am", my.age, "years old" together in the order lsitsed separated by comma.
